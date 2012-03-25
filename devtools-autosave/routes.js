6546ef6ad7bef96d70ad6aa3fa3a243968655397
Sukar YM <sukar@lab1521.com>
2012-03-26 01:56:32 +1200

    unang hakbang

---
+++ devtools-autosave/routes.js
@@ -0,0 +1,10 @@
+exports.routes = [
+    {
+        from: new RegExp('^http://localhost/'),
+        to: 'C:\/lab1521\/xampp\/htdocs/'
+    },
+	{
+        from: new RegExp('^http://contextlysitescripts\.contextly\.net/'),
+        to: 'C:\/lab1521\/xampp\/htdocs\/2011\/contextly\.net\/contextlysitescripts/'
+    },
+];
